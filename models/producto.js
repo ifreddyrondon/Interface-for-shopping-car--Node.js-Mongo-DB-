ec281b35a0d414c4ed51d146af2b63af7a3ddfe0
spantons <spantons@gmail.com>
2013-01-15 22:59:18 -0430

    Modificando models producto para que grabe en mongobd

--- models/producto.js
+++ models/producto.js
@@ -9,23 +9,23 @@ var producto_schema = new Schema({
   tamano						: 	{ type: Number,  required: true, trim: true	},
   cantidad_inicial	:  	{ type: Number,  required: true, trim: true	},
   cantidad_restante	:  	{ type: Number,	 required: true, trim: true	},
-  publicado					:		{ type: Boolean, required: true },
-  disponibilidad		:		{ type: Boolean, required: true },
-  tags							:		{ type: Array },
-  images						:	 	{ type: Array },
+  publicado					:		{ type: Boolean, default: false },
+  disponibilidad		:		{ type: Boolean, default: true },
+  tags							:		{ type: [String] },
+  images						:	 	{ type: [String] },
   creado  					: 	{ type: Date, default: Date.now },
   modificado				:	  { type: Date, default: Date.now },
-  variaciones				: 	{ type: Array },  
-  proveedor					: 	{ type: Array },
-  categoria					:  [{ type: Schema.Types.ObjectId, ref: 'categoria_schema' }],
+  variaciones				: 	{ type: [String] },  
+  proveedor					: 	{ type: [String] },
+  //categoria					:  [{ type: Schema.Types.ObjectId, ref: 'categoria_schema' }],
 });
 
 var categoria_schema = new Schema({
 	titulo						: 	{ type: String, required: true },
   descripcion				: 	{ type: String },
-  parent						: 	{ type: Schema.Types.ObjectId, ref: 'categoria_schema' },
-  images						: 	{ type: Array },
+  //parent						: 	{ type: Schema.Types.ObjectId, ref: 'categoria_schema' },
+  images						: 	{ type: [String] },
 });
-  
+
 module.exports = producto_schema;
-module.exports = categoria_schema;
+//module.exports = categoria_schema;
